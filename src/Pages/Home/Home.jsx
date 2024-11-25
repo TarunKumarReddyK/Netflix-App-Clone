3f30c0774fb8c71083e0e8c13db5e665c1cf519f
TarunKumarReddyK <133476621+TarunKumarReddyK@users.noreply.github.com>
2024-11-25 08:07:33 +0530

    Add files via upload

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './Home.css'
+import Navbar from '../../Components/Navbar/Navabr'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
+import TitleCards from '../../Components/TitleCards/TitleCards'
+import Footer from '../../Components/Footer/Footer'
+
+// import hero_title from '../../assets/hero_title.png'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      {/* <h2>Home</h2> */}
+      <Navbar/>
+
+      <div className="hero">
+        <img src={hero_banner} alt='' className='banner-img'/>
+        <div className="hero-caption">
+        <img src={hero_title} alt='' className='caption-img'/>
+        <p>Discovering his ties to a secret ancient
+           order, a young man living in modern Istanbul 
+           embarks on a quest to save the city from an immortal enemy.</p>
+          <div className="hero-btns">
+            <button className='btn'><img src={play_icon} alt='' />Play</button>
+            <button className='btn btn-dark'><img src={info_icon} alt='' />More Info</button>
+          </div>
+          < TitleCards />
+        </div>
+      </div>
+      <div className="more-cards">
+      < TitleCards  title={"Blockbuster Movies"} category={"top_rated"}/>
+      < TitleCards title={"Only on Netflix"} category={"popular"}/>
+      < TitleCards title={"Upcoming"} category={"upcoming"}/>
+      < TitleCards title={"To picks For You"} category={"now_playing"}/>
+
+      </div>
+
+      < Footer />
+    </div>
+  )
+}
+
+export default Home
